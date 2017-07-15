d984670f7ca3b0ebf7145a4e07056e6f331ceb3e
Magne Hustveit <magnehustveit@Magnes-MacBook-Pro-2.local>
2017-07-15 13:23:00 +0200

    Small fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $(window).scroll( function(){
-        $('.hideme').each( function(i){
+        $('.hideme').each( function(){
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_object ){
