3084d34bd2f65af615aee8a7154467067e5eb13b
Magne Hustveit <magnehustveit@Magnes-MacBook-Pro-2.local>
2017-08-15 01:29:56 +0200

    Fixed most urgent issues mentioned by thegge

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
         $('.hideme').each( function(){
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window > bottom_of_object ){
+            if( bottom_of_window > (bottom_of_object-400) ){
                 $(this).animate({'opacity':'1'},1500);
             }
         });
         $('.fadeinlist').each( function(i){
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window > bottom_of_object ){
+            if( bottom_of_window > (bottom_of_object-400) ){
                 $(this).delay(i * 300).animate({'opacity':'1'},1500);
             }
         });
